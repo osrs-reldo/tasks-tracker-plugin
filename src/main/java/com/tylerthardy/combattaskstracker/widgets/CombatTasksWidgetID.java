459fc4f4fde15922d0689ed3740dd875e5298435
Tyler Hardy <tylerthardy@gmail.com>
2021-08-19 20:27:39 -0400

    Add widget info

---
+++ src/main/java/com/tylerthardy/combattaskstracker/widgets/CombatTasksWidgetID.java
@@ -0,0 +1,49 @@
+package com.tylerthardy.combattaskstracker.widgets;
+
+public class CombatTasksWidgetID {
+    public static final int COMBAT_ACHIEVEMENTS_TASKS = 715;
+    public static final int COMBAT_ACHIEVEMENTS_OVERVIEW = 717;
+
+    static class CombatAchievementsTasks {
+        static final int CONTAINER = 1;
+        static final int CONTAINER_2 = 1;
+        static final int TASK_BAR = 3;
+        static final int HORIZONTAL_SEPARATOR = 4;
+        static final int VERTICAL_SEPARATOR = 5;
+        static final int TASK_LIST_CONTAINER = 6;
+        static final int TASK_LIST_SCROLLBAR = 7;
+        static final int TASK_LIST_VIEWPORT = 8;
+        static final int TASK_LIST = 9;
+        static final int TASK_LIST_TITLES = 10;
+        static final int TASK_LIST_MONSTERS = 11;
+        static final int TASK_LIST_EXPAND_BUTTONS = 14;
+        static final int TASK_LIST_TIER_ICONS = 15;
+        static final int TASK_LIST_NO_FILTER_RESULTS = 16;
+        static final int FILTERS_CONTAINER = 17;
+        static final int FILTERS_LABEL = 18;
+        static final int FILTER_TIER = 19;
+        static final int FILTER_TYPE = 20;
+        static final int FILTER_MONSTER = 21;
+        static final int FILTER_COMPLETED = 22;
+        static final int HAMBURGER_BUTTON = 23;
+        static final int HAMBURGER_DROPDOWN = 25;
+        static final int RESIZABLE_CONTAINER = 27;
+        static final int FILTER_DROPDOWN = 28;
+        static final int FILTER_DROPDOWN_BACKGROUND = 29;
+        static final int FILTER_DROPDOWN_TIER = 30;
+        static final int FILTER_DROPDOWN_TYPE = 31;
+        static final int FILTER_DROPDOWN_MONSTER = 32;
+        static final int FILTER_DROPDOWN_COMPLETED = 33;
+    }
+
+    static class CombatAchievementsOverview {
+        static final int CONTAINER = 2;
+        static final int TIERS_LABEL = 3;
+        static final int COMBAT_PROFILE_LABEL = 4;
+        static final int TIER_BUTTONS = 5;
+        static final int COMBAT_PROFILE_CONTAINER = 12;
+        static final int COMBAT_PROFILE_CONTAINER_2 = 13;
+        static final int HAMBURGER_BUTTON = 14;
+        static final int HAMBURGER_DROPDOWN = 16;
+    }
+}
