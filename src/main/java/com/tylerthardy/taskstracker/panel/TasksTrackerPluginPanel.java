8d68b04f4058e48ba493d681dd311c26226b8755
James Shelton <jshelton140@googlemail.com>
2022-01-18 23:44:56 +0000

    Change login panel state to use visibility

--- src/main/java/com/tylerthardy/taskstracker/panel/TasksTrackerPluginPanel.java
+++ src/main/java/com/tylerthardy/taskstracker/panel/TasksTrackerPluginPanel.java
@@ -4,26 +4,31 @@ import com.tylerthardy.taskstracker.TasksTrackerPlugin;
 import com.tylerthardy.taskstracker.panel.tabs.AllTaskListPanel;
 import com.tylerthardy.taskstracker.panel.tabs.TrackedTaskListPanel;
 import com.tylerthardy.taskstracker.tasktypes.Task;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import javax.swing.border.EmptyBorder;
 import javax.swing.SwingUtilities;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.game.SkillIconManager;
 import net.runelite.client.game.SpriteManager;
+import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.PluginPanel;
+import net.runelite.client.ui.components.PluginErrorPanel;
 
 @Slf4j
 public class TasksTrackerPluginPanel extends PluginPanel
 {
-	private final LoggedOutPanel loggedOutPanel;
 	public AllTaskListPanel allTasksPanel;
     public TrackedTaskListPanel trackedTaskListPanel;
     private TasksTrackerPlugin plugin;
     private final ClientThread clientThread;
     private final SpriteManager spriteManager;
     private SkillIconManager skillIconManager;
-	private LoggedInPanel loggedInPanel;
+	private final LoggedInPanel loggedInPanel;
 	private boolean loggedIn = false;
+	private final PluginErrorPanel errorPanel = new PluginErrorPanel();
+	private final LoggedOutPanel loggedOutPanel = new LoggedOutPanel();
 
 	public TasksTrackerPluginPanel(TasksTrackerPlugin plugin, ClientThread clientThread, SpriteManager spriteManager, SkillIconManager skillIconManager)
     {
@@ -33,8 +38,20 @@ public class TasksTrackerPluginPanel extends PluginPanel
         this.spriteManager = spriteManager;
         this.skillIconManager = skillIconManager;
 
-        loggedOutPanel = new LoggedOutPanel();
-        this.add(loggedOutPanel);
+		setBorder(new EmptyBorder(6, 6, 6, 6));
+		setBackground(ColorScheme.DARK_GRAY_COLOR);
+		setLayout(new BorderLayout());
+
+		loggedInPanel = new LoggedInPanel(plugin, clientThread, spriteManager, skillIconManager);
+		allTasksPanel = loggedInPanel.allTasksPanel;
+		trackedTaskListPanel = loggedInPanel.trackedTaskListPanel;
+		add(loggedInPanel, BorderLayout.NORTH);
+		loggedInPanel.setVisible(false);
+
+		// Add error pane @todo refactor LoggedInPanel into this so to not require super(false) and switch to errorPanel
+//		errorPanel.setContent("Task tracker", "Log into an account to track tasks.");
+//		add(errorPanel);
+		add(loggedOutPanel);
     }
 
     @Override
@@ -65,18 +82,13 @@ public class TasksTrackerPluginPanel extends PluginPanel
 
 		if (loggedIn != this.loggedIn)
 		{
-			this.removeAll();
 			if (loggedIn)
 			{
-				if (loggedInPanel == null)
-				{
-					loggedInPanel = new LoggedInPanel(plugin, clientThread, spriteManager, skillIconManager);
-					allTasksPanel = loggedInPanel.allTasksPanel;
-					trackedTaskListPanel = loggedInPanel.trackedTaskListPanel;
-				}
-				this.add(loggedInPanel);
+				loggedOutPanel.setVisible(false);
+				loggedInPanel.setVisible(true);
 			} else {
-				this.add(loggedOutPanel);
+				loggedInPanel.setVisible(false);
+				loggedOutPanel.setVisible(true);
 			}
 
 			validate();
