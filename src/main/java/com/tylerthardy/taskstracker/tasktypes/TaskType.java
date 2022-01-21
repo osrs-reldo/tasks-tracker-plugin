2e7083a0bd71c0ddfe2f0b45f1bcb88c30a81d11
Tyler Hardy <tylerthardy@gmail.com>
2022-01-20 19:29:20 -0500

    Remove leagues 1 & 2

--- src/main/java/com/tylerthardy/taskstracker/tasktypes/TaskType.java
+++ src/main/java/com/tylerthardy/taskstracker/tasktypes/TaskType.java
@@ -1,8 +1,6 @@
 package com.tylerthardy.taskstracker.tasktypes;
 
 import com.tylerthardy.taskstracker.tasktypes.combattask.CombatTask;
-import com.tylerthardy.taskstracker.tasktypes.league1.League1Task;
-import com.tylerthardy.taskstracker.tasktypes.league2.League2Task;
 import com.tylerthardy.taskstracker.tasktypes.league3.League3Task;
 import java.lang.reflect.Type;
 import lombok.AllArgsConstructor;
@@ -14,8 +12,6 @@ public enum TaskType
 {
 
 	COMBAT("Combat Tasks", "combat_tasks.min.json", CombatTask.class),
-	LEAGUE_1("League I: Twisted", "league1_tasks.min.json", League1Task.class),
-	LEAGUE_2("League II: Trailblazer", "league2_tasks.min.json", League2Task.class),
 	LEAGUE_3("League III: Shattered Relics", "league3_tasks.min.json", League3Task.class);
 	public final String displayString;
 	public final String dataFileName;
