2053d255953caed2335aa5f409c38a34473de88c
Tyler Hardy <tylerthardy@gmail.com>
2024-11-17 01:55:13 -0500

    Use develop branch

--- src/main/java/net/reldo/taskstracker/data/jsondatastore/JsonDataStore.java
+++ src/main/java/net/reldo/taskstracker/data/jsondatastore/JsonDataStore.java
@@ -2,5 +2,5 @@ package net.reldo.taskstracker.data.jsondatastore;
 
 public class JsonDataStore
 {
-	public static String baseUrl = "https://raw.githubusercontent.com/osrs-reldo/task-json-store/refs/heads/tasktype-json-schema";
+	public static String baseUrl = "https://raw.githubusercontent.com/osrs-reldo/task-json-store/refs/heads/develop";
 }
