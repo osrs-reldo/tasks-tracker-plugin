00b3a9e3ed4087122ce1443b80c4b702a4c5ba6b
James Shelton <jshelton140@googlemail.com>
2024-12-06 18:45:16 +0000

    Ignore profile changes until start up completed (#80)

--- src/main/java/net/reldo/taskstracker/TasksTrackerPlugin.java
+++ src/main/java/net/reldo/taskstracker/TasksTrackerPlugin.java
@@ -87,6 +87,7 @@ public class TasksTrackerPlugin extends Plugin
 	private NavigationButton navButton;
 	private RuneScapeProfileType currentProfileType;
 	private final Map<Skill, Integer> oldExperience = new EnumMap<>(Skill.class);
+	private boolean startUpCompleted = false;
 
 	@Inject	@Named("runelite.version") private String runeliteVersion;
 	@Inject private Gson gson;
@@ -149,6 +150,7 @@ public class TasksTrackerPlugin extends Plugin
 			.build();
 		clientToolbar.addNavigation(navButton);
 
+		startUpCompleted = true;
 		log.info("Tasks Tracker started!");
 	}
 
@@ -158,6 +160,7 @@ public class TasksTrackerPlugin extends Plugin
 		pluginPanel = null;
 		taskService.clearTaskTypes();
 		clientToolbar.removeNavigation(navButton);
+		startUpCompleted = false;
 		log.info("Tasks Tracker stopped!");
 	}
 
@@ -309,7 +312,10 @@ public class TasksTrackerPlugin extends Plugin
 	@Subscribe
 	public void onProfileChanged(ProfileChanged profileChanged)
 	{
-		reloadTaskType();
+		if (startUpCompleted)
+		{
+			reloadTaskType();
+		}
 	}
 
 	public void refresh()
